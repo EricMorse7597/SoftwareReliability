2394fc8ae270676a3c80814607e383859aa3db41
Or Brener <obrener@uoguelph.ca>
2024-09-19 20:24:34 -0400

    add duplicateObjectTest

--- A1/src/AppTest.java
+++ A1/src/AppTest.java
@@ -31,27 +31,30 @@ public class AppTest {
         Vector2.add(6);
         
         System.out.println("in appTest -- baseCaseTest()");
-        assertTrue("union is incorrect", App.union(Vector1, Vector2).equals(result));
+        assertTrue("union is incorrect in base case", App.union(Vector1, Vector2).equals(result));
     }
     
     @Test
-    public void test2() {
-        Vector<Object> answer = new Vector<Object>();
+    public void duplicateObjectTest() {
+        Vector<Object> result = new Vector<Object>();
         Vector<Object> Vector1 = new Vector<Object>();
         Vector<Object> Vector2 = new Vector<Object>();
 
-        answer.add(1);
-        answer.add(2);
-        answer.add(3);
-        answer.add(4);
-        answer.add(5);
+        result.add(1);
+        result.add(2);
+        result.add(3);
+        result.add(4);
+        result.add(5);
 
         Vector1.add(1);
         Vector1.add(2);
         Vector1.add(3);
+
         Vector2.add(3);
         Vector2.add(4);
         Vector2.add(5);
-        assertEquals(answer, App.union(answer, answer));
+        
+        System.out.println("in appTest -- duplicateObjectTest()");
+        assertTrue("union is incorrect with duplicate", App.union(Vector1, Vector2).equals(result));
     }
 }
