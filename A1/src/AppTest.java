cb0b57e279529c1a22c293226452df9d54792482
Eric <emorse@uoguelph.ca>
2024-09-19 20:00:42 -0400

    added test 2

--- A1/src/AppTest.java
+++ A1/src/AppTest.java
@@ -29,4 +29,25 @@ public class AppTest {
         Vector2.add(6);
         assertEquals(answer, App.union(answer, answer));
     }
+    
+    @Test
+    public void test2() {
+        Vector<Object> answer = new Vector<Object>();
+        Vector<Object> Vector1 = new Vector<Object>();
+        Vector<Object> Vector2 = new Vector<Object>();
+
+        answer.add(1);
+        answer.add(2);
+        answer.add(3);
+        answer.add(4);
+        answer.add(5);
+
+        Vector1.add(1);
+        Vector1.add(2);
+        Vector1.add(3);
+        Vector2.add(3);
+        Vector2.add(4);
+        Vector2.add(5);
+        assertEquals(answer, App.union(answer, answer));
+    }
 }
