db8ae78ecd5abf29bca9b5f6b8cf733922c1579d
Or Brener <obrener@uoguelph.ca>
2024-09-19 20:20:43 -0400

    update base case test (test case 1)

--- A1/src/AppTest.java
+++ A1/src/AppTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Vector;
 
@@ -9,25 +10,28 @@ public class AppTest {
     App ourApp = new App();
     
     @Test
-    public void test1() {
-        Vector<Object> answer = new Vector<Object>();
+    public void baseCaseTest() {
+        Vector<Object> result = new Vector<Object>();
         Vector<Object> Vector1 = new Vector<Object>();
         Vector<Object> Vector2 = new Vector<Object>();
 
-        answer.add(1);
-        answer.add(2);
-        answer.add(3);
-        answer.add(4);
-        answer.add(5);
-        answer.add(6);
+        result.add(1);
+        result.add(2);
+        result.add(3);
+        result.add(4);
+        result.add(5);
+        result.add(6);
 
         Vector1.add(1);
         Vector1.add(2);
         Vector1.add(3);
+
         Vector2.add(4);
         Vector2.add(5);
         Vector2.add(6);
-        assertEquals(answer, App.union(answer, answer));
+        
+        System.out.println("in appTest -- baseCaseTest()");
+        assertTrue("union is incorrect", App.union(Vector1, Vector2).equals(result));
     }
     
     @Test
