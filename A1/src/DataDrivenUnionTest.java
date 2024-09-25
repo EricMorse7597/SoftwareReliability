4f5c0ea69175b50cfeee87fbb61eae4602ec0987
Or Brener <obrener@uoguelph.ca>
2024-09-25 01:01:36 -0400

    convert tests into DataDrivenTests

---
+++ A1/src/DataDrivenUnionTest.java
@@ -0,0 +1,77 @@
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.*;
+import java.util.*;
+
+@RunWith (Parameterized.class) // This tells JUnit to use the Parameterized test runner, which allows the test
+// to be run multiple times with different input parameters.
+public class DataDrivenUnionTest {
+
+    private Vector<Object> vector1, vector2, result;
+
+    public DataDrivenUnionTest(Vector<Object> vector1, Vector<Object> vector2, Vector<Object> result) {
+        this.vector1 = vector1;
+        this.vector2 = vector2;
+        this.result = result;
+    }
+
+    
+  @Parameters  // This annotation is used to define the input values that the
+  // test will be executed with
+   
+  
+  public static Collection<Object[]> unionValues()  {
+    Vector<Object> result1 = new Vector<Object>();
+    Vector<Object> Vector11 = new Vector<Object>();
+    Vector<Object> Vector21 = new Vector<Object>();
+
+    result1.add(1);
+    result1.add(2);
+    result1.add(3);
+    result1.add(4);
+    result1.add(5);
+    result1.add(6);
+
+    Vector11.add(1);
+    Vector11.add(2);
+    Vector11.add(3);
+
+    Vector21.add(4);
+    Vector21.add(5);
+    Vector21.add(6);
+
+    Vector<Object> result2 = new Vector<Object>();
+    Vector<Object> Vector12 = new Vector<Object>();
+    Vector<Object> Vector22 = new Vector<Object>();
+
+    result2.add(1);
+    result2.add(3);
+    result2.add(4);
+    result2.add(5);
+
+    Vector12.add(1);
+    Vector12.add(null);
+    Vector12.add(3);
+
+    Vector22.add(3);
+    Vector22.add(4);
+    Vector22.add(5);
+    
+
+	return Arrays.asList (
+        new Object [][] { 
+            {Vector11, Vector21, result1}, {Vector12, Vector22, result2},} 
+        ); 
+  }
+
+    @Test
+    public void unionTest() {
+        System.out.println("vector 1: " + vector1);
+        System.out.println("vector 2: " + vector2);
+        System.out.println("vector union result: " + result);
+
+        assertTrue("Failed union test", App.union(vector1, vector2).equals(result));
+    }
+}
