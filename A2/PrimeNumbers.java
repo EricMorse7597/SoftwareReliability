c94fb3ebc02d86fc9e2f672cc6daf511dcf74eb5
Danindu Marasinghe <dmarasin@uoguelph.ca>
2024-10-07 17:32:09 -0400

    Start A2 Q2

---
+++ A2/PrimeNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class PrimeNumbers {
+    private List<Integer> primes = new ArrayList<Integer>();
+
+    public static void computePrimes(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
